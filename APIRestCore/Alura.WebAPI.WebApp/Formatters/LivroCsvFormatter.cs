1ce1d3171fc78eb6b51289b131a917eacacfecfa
rmcs87 <rmcs87@gmail.com>
2019-12-12 17:22:01 -0300

    Adição de um formatador personalisado para requisições pedindo formato csv.

---
+++ APIRestCore/Alura.WebAPI.WebApp/Formatters/LivroCsvFormatter.cs
@@ -0,0 +1,47 @@
+﻿using Alura.ListaLeitura.Modelos;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Net.Http.Headers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.WebAPI.WebApp.Formatters
+{
+    public class LivroCsvFormatter : TextOutputFormatter
+    {
+        public LivroCsvFormatter()
+        {
+            var csvMediType = MediaTypeHeaderValue.Parse("text/csv");
+            var appCsvMeiType = MediaTypeHeaderValue.Parse("application/csv");
+            SupportedMediaTypes.Add(csvMediType);
+            SupportedMediaTypes.Add(appCsvMeiType);
+
+            SupportedEncodings.Add(Encoding.UTF8);
+        }
+
+        protected override bool CanWriteType(Type type)
+        {
+            return type == typeof(LivroApi);
+        }
+
+        public override Task WriteResponseBodyAsync(OutputFormatterWriteContext context, Encoding selectedEncoding)
+        {
+            var livroEmCsv = "";
+
+            if (context.Object is LivroApi)
+            {
+                var livro = context.Object as LivroApi;
+
+                livroEmCsv = $"{livro.Titulo};{livro.Subtitulo};{livro.Autor};{livro.Lista};";
+            }
+
+            using (var escritor = context.WriterFactory(context.HttpContext.Response.Body, selectedEncoding))
+            {
+                return escritor.WriteAsync(livroEmCsv);
+            }
+
+        }
+    }
+}
