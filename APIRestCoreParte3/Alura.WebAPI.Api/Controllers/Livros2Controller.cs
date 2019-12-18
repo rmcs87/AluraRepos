40893058ecd966ddb390d586e011b28e5431ac03
rmcs87 <rmcs87@gmail.com>
2019-12-18 10:40:44 -0300

    Adição de Filtros via query para a API 2.0.

--- APIRestCoreParte3/Alura.WebAPI.Api/Controllers/Livros2Controller.cs
+++ APIRestCoreParte3/Alura.WebAPI.Api/Controllers/Livros2Controller.cs
@@ -1,5 +1,6 @@
 ﻿using Alura.ListaLeitura.Modelos;
 using Alura.ListaLeitura.Persistencia;
+using Alura.WebAPI.Api.Modelos;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
@@ -21,9 +22,12 @@ namespace Alura.ListaLeitura.Api.Controllers
         }
 
         [HttpGet]
-        public IActionResult ListaDeLivros()
+        public IActionResult ListaDeLivros([FromQuery] LivroFiltro filtro)
         {
-            var lista = _repo.All.Select(l => l.ToApi()).ToList();
+            var lista = _repo.All
+                .AplicaFiltro(filtro)
+                .Select(l => l.ToApi())
+                .ToList();
             return Ok(lista);
         }
 
